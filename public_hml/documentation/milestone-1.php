862b3d5ba131bf7a86778c3476c2a924506f1605
Jabari Farrar <jofarrar@gmail.com>
2017-04-27 06:35:15 -0600

    updated the html file with more verbage and made corrections

--- public_hml/documentation/milestone-1.php
+++ public_hml/documentation/milestone-1.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
+
 <html lang="en">
+
 	<head>
 
 		<meta charset="utf-8"/>
@@ -23,27 +25,36 @@
 				<p><strong>Goal:</strong> The goal of this website is to acquire an internship, freelance work or employment.</p>
 
 				<p><strong>Persona:</strong></p>
+
 				<p>Name: Lester Boomhackel</p>
+
 				<p>Age: 56 years old</p>
+
 				<p>Education: M.S. in Human Resources Management</p>
-				<p>Technology: smartphone (Samsung S8), laptop (Macbook Air), and a personal computer at work (Dell Inspiron) </p>
+
+				<p>Technology: smartphone (Samsung Galaxy S8), laptop (Macbook Air), and a personal computer at work (Dell Inspiron) </p>
+
 				<p>Attitudes and Behaviors: Lester is short on time and is the only HR representative for this company of 15 people.
 				Lester is an avid user of LinkedIn and likes to look at the links included on a person's LinkedIn page if the candidate
 				is a potential match for the positon.</p>
-				<p>Frustrations: Lester needs to hire 2-3 people for web application positions for a scientific software company;
-				however, these people need to be able understand not only web development tools, but they need to have atechnical background to understand the content of the web 				applications. He must hire the proper candidates within
-				two weeks so they have time to ramp up before the new projects are kicked off.</p>
+
+				<p>Frustrations and Needs: Lester needs to hire 2-3 people for web application positions for a scientific software company;
+				however, these people need to be able understand not only web development tools, but they need to have a technical background to understand the content of the web 				applications. Most of the candidates he finds on LinkedIn do not have a technical background. He must hire the proper candidates within two weeks so they have time to 				ramp up before the new projects are kicked off.</p>
+
 				<p>Goal: Since the company is composed of a total of 15 employees, Lester is burdened with hiring for other open
 				positions within the company as well. Lester has very little time to make a decision as to whether a potential
 				candidate is a fit or not. Lester must get this information from a quick glance at a site or he moves on.</p>
+
 				<p>User Story: As a user I need to quickly access the candiates web developmenet skills as well as their ability to understand the technical content of the web 				applications they will be building.</p>
 
 				<p><strong>Use Case:</strong></p>
+
 				<p>Lester is perusing through LinkedIn using his Mozilla Firefox browser at 7:45 am after searching for local candidates using a few keywords.
 				He knows he has to present a few candidates to the hiring manager by 1:00 pm today in spite of his meetings
 				this morning from 10:00 am to 11:30 am, and has no time to waste.</p>
+
 				<p> Lester has viewed my LinkedIn page and sees a profile with a strong background in chemistry and web development experience.
-				His interest is piqued and he views my Github and personal website links</p>
+				His interest is piqued and he views my Github and personal website links and would like to contact me by phone.</p>
 
 	</body>
 
