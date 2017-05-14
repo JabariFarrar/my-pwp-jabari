ad2bfdb3d05e6da40ca478e48b57c346b7d29293
Jabari Farrar <jofarrar@gmail.com>
2017-05-14 16:08:49 -0600

    added verbage to the file

---
+++ public_hml/documentation/milestone-2.php
@@ -0,0 +1,31 @@
+<DOCTYPE html>
+	<html lang="eng">
+		<head>
+			<meta charset="UTF-8">
+			<title>PWP Milestone 2alpha</title>
+		</head>
+		<body>
+			<header>
+
+				<h1>PWP Milestone 2alph</h1>
+
+			</header>
+
+				<p><em> Jabari O. Farrar  Chemist/Software Developer:<em></em> Below this title, I would like to give the user a synopsis about me, including my background in chemistry, the industries I worked in and my recent transition to software development </em> </p>
+
+		</body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	</html>
