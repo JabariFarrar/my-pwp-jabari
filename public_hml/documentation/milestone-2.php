dbf1f2483710c75a530f7f73552fa4d2ac659c2b
Jabari Farrar <jofarrar@gmail.com>
2017-05-14 17:03:43 -0600

    added verbage to the file and corrected some errors

--- public_hml/documentation/milestone-2.php
+++ public_hml/documentation/milestone-2.php
@@ -7,11 +7,15 @@
 		<body>
 			<header>
 
-				<h1>PWP Milestone 2alph</h1>
+				<h1>PWP Milestone 2alpha</h1>
 
 			</header>
 
-				<p><em> Jabari O. Farrar  Chemist/Software Developer:<em></em> Below this title, I would like to give the user a synopsis about me, including my background in chemistry, the industries I worked in and my recent transition to software development </em> </p>
+				<p><em> Jabari O. Farrar  Chemist/Software Developer:<em></em> Below this title, I would like to give the user a synopsis about me, including my background in chemistry, the industries  I worked in and my recent transition to software development.</p>
+				<p><em>Professional Experience:</em> I would like to list my professional experience so that the user can see my work history and the breadth of my responsibilities.</p>
+				<p><em>Publications: </em> By showing my publications, this is a way of legitimizing what I have done in academia and in industry. Many companies would like to see what sort of peer-reviewed articles you have published, which is used as a vetting process for many employers.</p>
+				<p><em>Portfolio:</em> The portfolio will serve to show potential employers that I am serious about coding and give them examples of my work.</p>
+				<p><em>Contact Me:</em> This will allow potential employers to reach me directly and allow them to enquire.</p>
 
 		</body>
 
