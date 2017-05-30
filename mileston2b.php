94058f40c7a992bf8898d26bd580faf115c1a0e9
Jabari Farrar <jofarrar@gmail.com>
2017-05-30 07:33:25 -0600

    added images and verbage could not get navbar to work. Not much time left.

---
+++ mileston2b.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Personal Website">
+		<meta name="author" content="Jabari O. Farrar">
+
+		<title>Jabari O. Farrar</title>
+
+		<!-- Latest compiled and minified Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Font Awesome -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css"
+				rel="stylesheet">
+
+		<!-- MY Custom CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
+
+		<!-- jQuery Form, Validate, Additional Methods -->
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/jquery.validate.min.js"
+				  type="text/javascript"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/additional-methods.min.js"
+				  type="text/javascript"></script>
+
+		<!-- Your JavaScript Form Validator -->
+		<script src="js/form-validate.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
+
+		<!-- Custom CSS -->
+		<link href="css/scrolling-nav.css" rel="stylesheet">
+
+		<!-- Scrolling Nav JavaScript -->
+		<script src="js/jquery.easing.min.js"></script>
+		<script src="js/scrolling-nav.js"></script>
+
+	</head>
+
+	<!-- The #page-top ID is part of the scrolling feature - the data-spy and data-target are part of the built-in Bootstrap scrollspy function -->
+
+	<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+		<header>
+
+			<!-- Navigation -->
+			<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+				<div class="container">
+					<div class="navbar-header page-scroll">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand page-scroll" href="#page-top">
+							<div class="name-button">
+								 <span>Jabari O. Farrar</span>
+							</div>
+						</a>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse navbar-ex1-collapse">
+						<ul class="nav navbar-nav navbar-right">
+							<!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+							<li class="hidden">
+								<a class="page-scroll" href="#page-top"></a>
+							</li>
+							<li>
+								<a class="page-scroll" href="#About Me">About</a>
+							</li>
+							<li>
+								<a class="page-scroll" href="#Professional Experience">Experience</a>
+							</li>
+							<li>
+								<a class="page-scroll" href="#Publications">Publications</a>
+							</li>
+							<li>
+								<a class="page-scroll" href="#Portfolio">Portfolio</a>
+							</li>
+							<li>
+							<a class="page-scroll" href="#Contact">Contact</a>
+							</li>
+							<li>
+								<a href="https://twitter.com/jofarrar_1" target="_blank"><i class="fa fa-twitter fa-lg" aria-hidden="true"></i></a>
+								</li>
+							<li>
+								<a href="https://www.linkedin.com/in/jabari-farrar/" target="_blank"><i class="fa fa-linkedin fa-lg" aria-hidden="true"></i></a>
+							</li>
+							<li>
+								<a href="https://github.com/JabariFarrar/" target="_blank"><i class="fa fa-github fa-lg" aria-hidden="true"></i></a>
+							</li>
+						</ul>
+					</div>
+					<!-- /.navbar-collapse -->
+				</div>
+				<!-- /.container -->
+			</nav>
+
+		</header>
+		<main>
+			<!-- About Section -->
+			<section id="about" class="about-section">
+
+				<div class="container">
+
+					<div class="row">
+
+						<div class="col-lg-12">
+
+							<h1 class="section-title">About <span>Section</span></h1>
+
+							<img class="my-photo img-circle" src="images/computerTable.png"/>
+							<p> Jabari O. Farrar  Chemist/Software Developer</p>
+
+							<p>Hello and welcome to my webpage. My name is Jabari Farrar and I have a passion for science.
+								I have a PhD in Analytical Chemistry, specifically surface analysis of heterogeneous polymer blends, and I have a background in image analysis.
+								I am a junior web developer that would like help YOU merge science and technology.
+							</p>
+
+						</div>
+
+					</div>
+
+				</div>
+
+			</section>
+
+			<!-- Professional Experience Section -->
+			<section id="experience" class="Professional Experience">
+
+				<div class="container">
+
+					<div class="row">
+
+						<div class="col-lg-12">
+
+							<h1 class="section-title">Experience <span>experience</span></h1>
+							<ul>
+								<li>
+									Mayne Pharma, Greenville, NC
+									Performed method development, method validation, method transfers and analytical
+									testing in support of pharmaceutical product development and pharmaceutical
+									manufacturing using HPLC, UPLC and GC.
+								</li>
+								<li>
+									Northrop Grumman Aerospace Systems, Redondo Beach, CA
+									Performed chemical and material analyses, interpretation and consultation in support
+									of military and scientific programs at Northrop Grumman.
+								</li>
+								<li>
+									Procter & Gamble (Contractor), Cincinnati OH
+									Collaborated with members of P&G’s Fabric and Home Care Technical Division in an
+									effort to determine the best modified starch biopolymers, a cost-saving renewable
+									resource, to use in place of expensive petroleum-based polymers in laundry detergent
+									formulations.
+								</li>
+								<li>
+									Research Triangle Institute International, RTP, NC
+									Provided analytical separations support for ongoing National Toxicology Programs
+									conducted at Research Triangle Institute International.
+								</li>
+
+							</ul>
+
+						</div>
+
+					</div>
+
+				</div>
+
+			</section>
+			<!-- Publications Section -->
+			<section id="publications" class="Section">
+
+				<div class="container">
+
+					<div class="row">
+
+						<div class="col-lg-12">
+
+							<h1 class="section-title">Publications <span> Section </span></h1>
+
+							<ul>
+								<li>Dissertation: AFM and XPS Data Correlation</li>
+
+								<li>Kateryna Artyushkova, Jeffrey Fenton, Jabari Farrar and Julia Fulghum Multitechnique Fusion of
+									Imaging Data for Heterogeneous Materials, Edited by Yufeng Zheng, June 2011</li>
+
+								<li>Sergey Dubin, Scott Gilje , Kan Wang, Vincent C. Tung, Kitty Cha, Anthony S. Hall,
+									Jabari Farrar , Rupal Varshneya, Yang Yang , Richard B. Kaner, “A One-Step,
+									Solvothermal Reduction Method for Producing Reduced Graphene Oxide Dispersion
+									in Organic Solvents” ACS Nano, 4(7), 3845-3852, 2010 </li>
+
+								<li>Scott Gilje , Sergey Dubin,Aireza Badkhshan, Jabari Farrar , Stephen A.
+									Danczyk ,Richard B. Kaner, “Photothermal Deoxygenation of Graphene Oxide for
+									Patterning and Distributed Ignition Applications” Advanced Materials, 22(3),
+									419-423, 2010</li>
+
+								<li>Artyushkova, K., Farrar, J.O., Fulghum, J.E., “Data Fusion of XPS and AFM Images
+									for Chemical Phase Identification in Polymer Blends” Surface and InterfaceAnalysis,
+									41(2), 119-126, 2009
+								</li>
+
+								<li>Artyushkova K., Ferryman A., Farrar, J., Fulghum J.E., “Laboratory XPS
+									imaging:Improved procedures and data analysis” Journal of Surface Analysis, 9(3),
+									332-338, 2002
+								</li>
+
+							</ul>
+
+						</div>
+
+					</div>
+
+				</div>
+
+			</section>
